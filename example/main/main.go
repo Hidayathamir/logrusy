856ea44bbfe8273fd2e9b852c6b7472596900329
Hidayat Hamir <hidayat.03@erajaya.com>
2023-12-06 01:10:06 +0700

    fix: update example

--- example/main/main.go
+++ example/main/main.go
@@ -12,9 +12,9 @@ type User struct {
 func main() {
 	u := User{Name: "hidayat", Age: 24}
 	logrusy.Info(u)
-	// {"level":"info","msg":"{hidayat 24}","parsed_msg":{"Name":"hidayat","Age":24},"time":"2023-12-06T00:48:33+07:00"}
+	// {"file":"/home/hidayat/data-d/personal-project/logrusy/example/main/main.go:14","func":"main.main","level":"info","msg":"{hidayat 24}","parsed_msg":{"Name":"hidayat","Age":24},"time":"2023-12-06T01:07:39+07:00"}
 
 	numbers := []int{0, 1, 2, 3, 4, 5}
 	logrusy.Info(numbers)
-	// {"level":"info","msg":"[0 1 2 3 4 5]","parsed_msg":[0,1,2,3,4,5],"time":"2023-12-06T00:48:33+07:00"}
+	// {"file":"/home/hidayat/data-d/personal-project/logrusy/example/main/main.go:18","func":"main.main","level":"info","msg":"[0 1 2 3 4 5]","parsed_msg":[0,1,2,3,4,5],"time":"2023-12-06T01:07:39+07:00"}
 }
